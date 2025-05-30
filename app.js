59e0f4e3016a19e0b811d2f0254158dc093dfbf0
Jonathan Davis <cartman61616@gmail.com>
2025-05-30 00:34:39 +0000

    Add hello world app for argo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port);
+console.log(`App listening at http://localhost:${port}`);
+
+
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.get('/', (req, res) => {
+    const name = ProcessingInstruction.env.NAME || 'World';
+    res.send(`Hello ${name}!`);
+});
